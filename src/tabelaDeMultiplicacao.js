124f78c8eafd801f1412017601478b814110f74f
guerreirografico <guerreirografico@gmail.com>
2021-10-20 23:00:53 -0300

    Adiciona as funções (tabelaMultiplicação e tabela Multiplicação Dinâmica)

--- src/tabelaDeMultiplicacao.js
+++ src/tabelaDeMultiplicacao.js
@@ -1,9 +1,26 @@
-function nomeDaFuncao() { 
-    // Substitua "nomeDaSuaFuncao" por um nome mais apropriado
-    // TODO: escrever dois loops aninhados para desenhar o array bidimensional e monstar o restante da tabela no console.
+// Tabela multiplicação estática.
+function tabelaMultiplicacao() { 
     let x = [];
     for (let i = 0; i <= 10; i++) {
         x[i] = [];
+        for (let j = 0; j <= 10; j++) {
+            x[i][j] = i * j;
+        }
     }
-    console.table(x)
+    console.table(x);
 }
+tabelaMultiplicacao()
+
+
+// Tabela multiplicação dinâmica.
+function tabelaMultiplicacaoDinamica(n) {
+    let x = [];
+    for (let i = 0; i <= n; i++) {
+        x[i] = [];
+        for (let j = 0; j <= n; j++) {
+            x[i][j] = i * j;
+        }
+    }
+    console.table(x);
+}
+tabelaMultiplicacaoDinamica(5)
